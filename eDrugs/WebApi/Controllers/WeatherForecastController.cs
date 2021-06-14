0a6d5c58b702893d0e22fac79ac6f1a941ab794e
Al-amen <alamen15@cse.pstu.ac.bd>
2021-06-14 13:13:00 +0600

    int web api

---
+++ eDrugs/WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        //private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController()
+        {
+            
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var rng = new Random();
+            return "Hello Bangladesh";
+            
+        }
+    }
+}
